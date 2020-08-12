18e1be58f60aa7c64733df20bd1800b6d70710bd
James Bowden <jamesbowd@gmail.com>
2020-08-12 21:18:09 +0100

    removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr) {
+  let args = Array.prototype.slice.call(arguments, 1);
 
-}
+  for (let i = 0; i < arr.length; i++) {
+    for (item of args) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  return arr;
+};
 
-module.exports = removeFromArray
+module.exports = removeFromArray;
