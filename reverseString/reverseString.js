2c7a46dd7b5b3ade5a94c69c1c77c696f6e2b8bb
James Bowden <jamesbowd@gmail.com>
2020-08-12 20:52:10 +0100

    reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    outputString = ""
+    for (let i = str.length-1; i >=0; i--) {
+        outputString += str[i];
+    }
+    return outputString;
 }
 
 module.exports = reverseString
