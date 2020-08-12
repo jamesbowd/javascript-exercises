b013ddea5c559e7688dc73a8b89f98bf80455318
James Bowden <jamesbowd@gmail.com>
2020-08-12 20:46:42 +0100

    repeatString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    returnString = "";
 
+    if (num < 0) {
+        return "ERROR";
+    }else {
+        for (let i = 0; i < num; i++) {
+            returnString += str;
+        }
+    }
+    return returnString;
 }
 
 module.exports = repeatString
