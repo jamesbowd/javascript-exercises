5a2da3ae5f42e5440a40285f760d8b40c54bf0ae
James Bowden <jamesbowd@gmail.com>
2020-08-17 20:30:39 +0100

    sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (firstInt, secondInt) {
+  let total = 0;
 
-}
+  if (firstInt < 0 || secondInt < 0) {
+    return "ERROR";
+  }
 
-module.exports = sumAll
+  if (typeof firstInt != "number" || typeof secondInt != "number") {
+    return "ERROR";
+  }
+
+  if (firstInt < secondInt) {
+    for (let i = firstInt; i <= secondInt; i++) {
+      total += i;
+    }
+    return total;
+  }
+
+  if (secondInt < firstInt) {
+    for (let i = secondInt; i <= firstInt; i++) {
+      total += i;
+    }
+    return total;
+  }
+};
+
+sumAll(10, "90");
+
+module.exports = sumAll;
