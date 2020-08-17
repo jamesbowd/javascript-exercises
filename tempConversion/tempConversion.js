9070c8b579cfa5fac11af65c69c427ca29f0bcb2
James Bowden <jamesbowd@gmail.com>
2020-08-17 21:01:42 +0100

    tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,18 @@
-const ftoc = function() {
+const ftoc = function (fInt) {
+  // [°C] = ([°F] − 32) × ​5⁄9
+  let cInt = ((fInt - 32) * 5) / 9;
+  let roundedCInt = Math.round(cInt * 10) / 10;
+  return roundedCInt;
+};
 
-}
-
-const ctof = function() {
-
-}
+const ctof = function (cInt) {
+  // [°F] = [°C] × ​9⁄5 + 32
+  let fInt = (cInt * 9) / 5 + 32;
+  let roundedFInt = Math.round(fInt * 10) / 10;
+  return roundedFInt;
+};
 
 module.exports = {
   ftoc,
-  ctof
-}
+  ctof,
+};
