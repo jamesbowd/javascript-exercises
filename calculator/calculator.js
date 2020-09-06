7b6e41ee8daddeaefed420471cd2005040d52202
James Bowden <jamesbowd@gmail.com>
2020-09-06 20:49:52 +0100

    calculator exercice

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,47 @@
-function add () {
-	
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract () {
-	
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function sum () {
-	
+function sum(arr) {
+  let total = 0;
+  for (let num of arr) {
+    total += num;
+  }
+  return total;
 }
 
-function multiply () {
-	
+function multiply(arr) {
+  let total = 1;
+  for (let num of arr) {
+    total *= num;
+  }
+  return total;
 }
 
-function power() {
-	
+function power(num1, num2) {
+  return num1 ** num2;
 }
 
-function factorial() {
-	
+function factorial(num) {
+  var total = 1;
+  if (num === 0) {
+    return 1;
+  }
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
