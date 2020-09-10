2fdc6c7aefd7977014da60200d5daffa3eefc759
James Bowden <jamesbowd@gmail.com>
2020-09-10 20:10:25 +0100

    palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function() {
+const palindromes = function (testString) {
+  testString = testString.replace(/[^\w]|_/g, "");
+  testString = testString.toLowerCase();
 
-}
+  for (let i = 0; i < testString.length; i++) {
+    let charOne = testString[i];
+    let charTwo = testString[testString.length - 1 - i];
 
-module.exports = palindromes
+    if (charOne !== charTwo) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = palindromes;
+
+// palindromes("racecar");
